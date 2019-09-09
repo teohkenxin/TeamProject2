e09c1bef81f11c42bfd9d45b53bb29112ab3bb50
Kyrill <kyrill.spiegl@gmail.com>
2019-09-09 13:58:29 +0900

    fixed errors in main method and imported base package into MyApp class

--- main/MyApp.java
+++ main/MyApp.java
@@ -1,5 +1,5 @@
 package main;
-import TeamProject2.base;
+import base.*;
 /**
  * Write a description of class MyApp here.
  *
--- myInterface/Shape.java
+++ myInterface/Shape.java
@@ -1,22 +1,23 @@
 package myInterface;
 
 
+
 /**
- * 상수와 추상메소드를 가지고있는 인터페이스입니다.
+ * �?수와 추�?메소드를 가지고있는 �?�터페�?�스입니다.
  *
- * @author (스피겔 크릴 2017605038, 테오 켄신 2018771052)
+ * @author (스피겔 �?�릴 2017605038, 테오 켄신 2018771052)
  * @version (2019/09/04)
  */
 public interface Shape
 {
-    final double PI = 3.14; // 상수
+    final double PI = 3.14; // �?수
     
     
-    void draw(); // 도형을 그리는 추상 메소드
+    void draw(); // �?�형�?� 그리는 추�? 메소드
     
-    double getArea(); // 도형의 면적을 리턴하는 추상 메소드
+    double getArea(); // �?�형�?� 면�?�?� 리턴하는 추�? 메소드
     
-    default public void redraw(){ // 디폴트 메소드
+    default public void redraw(){ // 디�?�트 메소드
         System.out.print("--- 다시 그립니다. "); 
         draw(); 
     } 
