4f3b998a73455d09d4d4734a1834e829067e74b8
teohkenxin <teohkenxin@hotmail.com>
2019-09-09 13:30:29 +0900

    Initial sharing of project

---
+++ base/Circle.java
@@ -0,0 +1,23 @@
+package base;
+
+
+/**
+ * Write a description of class Circle here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Circle
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class Circle
+     */
+    public Circle(int x, int y, double radius)
+    {
+        this.x
+    }
+
+}
