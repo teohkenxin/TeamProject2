da43360a32d47632f7129531d3e2ca002b209d79
Kyrill <kyrill.spiegl@gmail.com>
2019-09-09 13:52:02 +0900

    outline for MyApp main method

---
+++ main/MyApp.java
@@ -0,0 +1,22 @@
+package main;
+import TeamProject2.base;
+/**
+ * Write a description of class MyApp here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class MyApp
+{
+    public static void main(String[] args){
+        Circle c1 = new Circle(2, 3, 5);
+        Circle c2 = new Circle(2, 3, 10);
+        System.out.println("원1 : " + c1);
+        System.out.println("원2 : " + c2);
+        if(c1.equals(c2)){
+            System.out.println("같은 원");
+        }else{
+            System.out.println("서로 다른 원");
+        }
+    }
+}
